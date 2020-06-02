b31b88b8a9e9838b44c59c26b5943583b512c556
Jonathan Niles <jonathanwniles@gmail.com>
2020-06-02 13:43:10 +0100

    chore: add debug logs to save.js
    Adds debug logging to save.js.

--- save.js
+++ save.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
+const debug = require('debug')('imaworldhealth:sms-odk:save');
 
 function saveSMS(jsonSMS) {
   const today = moment(new Date()).format('Y-MM-d');
   const hour = moment(new Date()).format('H-m-s');
 
-  const file = `${(jsonSMS.From || '')}-${hour}--${(Math.floor(Math.random() * 100))}`;
+  const file = `${(jsonSMS.From || '')}-${hour}-${(Math.floor(Math.random() * 100))}`;
 
   try {
-    fs.mkdirSync(path.resolve(__dirname, `./sms/${today}`));
+    fs.mkdirSync(path.resolve(__dirname, `./sms/${today}`), { recursive: true });
   } catch (error) {
+    debug('An error occurred creating directory: ', error);
   }
+
   fs.writeFileSync(path.resolve(__dirname, `./sms/${today}/${file}.json`), JSON.stringify(jsonSMS));
 }
 
